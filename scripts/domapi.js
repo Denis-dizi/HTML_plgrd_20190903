cbb712f0ea139973c24c86fb3a01d9a6e4529549
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-11-22 01:23:45 +0200

    before adding for loop

--- scripts/domapi.js
+++ scripts/domapi.js
@@ -1,7 +1,14 @@
 
 
-
+//first we find the element that we will add new elements
 const myApp = document.getElementById("app-1");
+
+//we can style the element
 myApp.style.backgroundColor = "aqua";
 myApp.style.height = "70vh";
 
+//we can add new elements (children)
+//first we create a new element NOT attached to anything
+const newP = document.createElement('p');
+newP.innerText = "Lorem";
+myApp.appendChild(newP);
