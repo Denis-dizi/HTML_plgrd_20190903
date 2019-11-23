a29244e750e196d0ff0ee9a61d3d0a857cf555e8
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-11-23 03:06:47 +0200

    chabged styling

--- scripts/domapi.js
+++ scripts/domapi.js
@@ -2,12 +2,14 @@
 //W3D5 (2:13:) W4D1 (0:29:)
 
 const CFG = {   //W4D1 (0:39:) CFG - config
-    MAXPAR : 5
+    MAXPAR : 5,
+    backgroundColor: "aqua",
+    height: "70vh"
 }
 
 function main() { //(2:36:)
     //first we find the element that we will add new elements (2:14:)
-    const myApp = styleMainParent("#app-1");         //W4D1 (0:52:) changed radicaly
+    const myApp = styleMainParent("#app-1", CFG);  //W4D1 (0:52:) changed radicaly
 
     createChildren("#app-1");
     setTimeout(deleteAllChildren, 5000, "#app-1"); //W4D1 (0:33:)
@@ -17,8 +19,8 @@ function main() { //(2:36:)
 function styleMainParent(selector, mystyle) {        //W4D1 (0:48:)
     //const myApp = document.getElementById("app-1");
     const myApp = document.querySelector(selector);
-    myApp.style.backgroundColor = "aqua";
-    myApp.style.height = "70vh";
+    myApp.style.backgroundColor = mystyle.backgroundColor;
+    myApp.style.height = mystyle.height;
     return myApp;
 }
 
