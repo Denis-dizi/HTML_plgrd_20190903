eac00cf5413bd507ea3f19dcc0d09983a450a225
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-11-23 02:30:24 +0200

    before changes (cleaning)

--- scripts/domapi.js
+++ scripts/domapi.js
@@ -1,9 +1,14 @@
 
-//W3D5 (2:13:)
+//W3D5 (2:13:) W4D1 (0:29:)
+
+const CFG = {   //W4D1 (0:39:) CFG - config
+    MAXPAR : 10
+}
+
 function main() { //(2:36:)
     //first we find the element that we will add new elements (2:14:)
     const myApp = document.getElementById("app-1");
-    const MAXPAR = 10 //(2:30:)
+    //const MAXPAR = 10 //(2:30:)
     //we can style the element
     myApp.style.backgroundColor = "aqua";
     myApp.style.height = "70vh";
@@ -14,13 +19,13 @@ function main() { //(2:36:)
     const newP = document.createElement('p'); // (2:19:)
     newP.innerText = "Lorem";
     myApp.appendChild(newP);*/
-    for (let i = 0; i < MAXPAR; i++ ){ //(2:27:)
+    for (let i = 0; i < CFG.MAXPAR; i++ ) { //(2:27:)
         const newP = document.createElement('p');
         newP.id = "p-"+i;
         newP.classList.add("my-text");
         newP.style.height = (i*2 + 20) + "px"; //(2:31:)
         newP.style.fontSize = 15 + i*3 + "px"; //(2:46:)
-        newP.style.backgroundColor = "hsl("+i*360/MAXPAR+", 30%, 40%)"; //(2:33:)
+        newP.style.backgroundColor = "hsl("+(i*360/CFG.MAXPAR)+", 30%, 40%)" //(2:33:)
         newP.innerText = "Lorem " +i;
         myApp.appendChild(newP);
     }
@@ -28,10 +33,14 @@ function main() { //(2:36:)
     //if we want to delete immediately: deleteAllChildren('#app-1'); //(0:30:)
 }
 
+function createAllChildren(selector){    //(0:36:)
+    const parent = document.querySelector(selector);
+}
+
 function deleteAllChildren(selector) {  //(0:29:)
-    const element = document.querySelector(selector)
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+    const parent = document.querySelector(selector)
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
     console.log("All children should be ---");
 }
