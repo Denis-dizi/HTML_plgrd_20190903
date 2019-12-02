10199a6a6879bbe1ab7ca8ab91b083e772eedce5
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-12-02 18:48:24 +0200

    alter without jQuery

--- scripts/myjq.js
+++ scripts/myjq.js
@@ -1,3 +1,25 @@
 //created W4D2 (1:35:)
-$("h1").css("background-color", "green");
-$(".my-txt").css("background-color", "aqua");
+
+/* anonima funkcija W4D2 (1:46:)
+jQuery mega funkcijai ($) esam iedevuši dokumentu; 
+.ready - specify a func to execute after the DOM is ready 
+(ready metode pieprāsa funkciju()): */
+
+// $(document).ready(function() {  
+//     $("h1").css("background-color", "green");
+//     $(".my-txt").css("background-color", "aqua");
+//   });
+
+
+//run main only when document is ready
+//$(document).ready(main);
+
+// alternativa bez jQuery/ in a modern browser it would be something like this //W4D2 (1:52)
+document.addEventListener('DOMContentLoaded', main, false);
+
+function main() {  
+    console.log("Running main!");
+    $("h1").css("background-color", "green");
+    $(".my-txt").css("background-color", "aqua");
+  };
+
