09c2f8051623661c003bed4dd3c6066da5da4a6f
Deniss Zhuravskis <davidovzzz@gmail.com>
2019-12-09 21:34:05 +0200

    example: variables & .getElementById();

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,14 @@ console.log("Going to alert");
 // alert ("Achtung!");
 console.log ("I have alerted!");
 document.body.style.backgroundColor = "lightblue";
+
+var a =20;
+var b = 30 - a;
+var c = b * a;
+console.log("My variables: "+ a, b, c);
+console.log(a, b, c);
+
+var myDiv = document.getElementById('output');
+//Javascript lets me add strings with numbers,
+//meaning JS converts number to string then concatanetes two strings
+myDiv.innerText = "My Variables: " + a + " : " + b + " : " +c;
