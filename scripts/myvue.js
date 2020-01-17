250402e51adff19b5e15636ede2f638e60961b15
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-01-17 22:56:03 +0200

    updated Vue lection

--- scripts/myvue.js
+++ scripts/myvue.js
@@ -1,29 +1,31 @@
 console.log("Vue script dz");                        //W4D5 (2:15:)
 
-const app = new Vue({
+const app = new Vue({                                //  https://vuejs.org/v2/guide/#Declarative-Rendering
     el : "#app1", //here we define root element (parent) where we will hold our app
     data : {
         message : "RCS",
         warning : "leopard",
         created : "This was made on " + new Date().toLocaleString(), //W4D5 (2:23:)(24)
         seen : true,                                 //W4D5 (2:32:)
-        todos: [                                     //W4D5 (2:34:)
-            { text: 'Learn JavaScript' },
+        todos: [                                     //W4D5 (2:34:)  cikls iziet cauri elementiem
+            { text: 'Learn JavaScript' },            // https://vuejs.org/v2/guide/#Conditionals-and-Loops
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
           ]
 
     },
     methods : {                                      //W4D5 (2:40:)
-        reverseMessage : function() {
+        reverseMessage : function() {                // https://vuejs.org/v2/guide/#Handling-User-Input
             //another bit of magic this.message is mapped to this.data.message
             console.log("Reversing Message");
             this.message = this.message.split('').reverse().join('');
         }
     }
-
-
-
 })
 
 //  https://vuejs.org/v2/guide/
+
+// Console manipulations:
+//app.message = "Some Mesage"                       //W4D5 (2:28:)
+//app.warning = "tiger"
+//app.seen = false - disapear all with "seen"
