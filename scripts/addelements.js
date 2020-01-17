2b8b9e7e39288c3a000d3a4e6b1494c283c7a910
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-01-17 23:50:43 +0200

    update AddElements timestamps W5D1

--- scripts/addelements.js
+++ scripts/addelements.js
@@ -3,27 +3,26 @@ main();
 
 
 function main() {
-    const addBtn = document.querySelector(".addelements");
-        //
+    const addBtn = document.querySelector(".addelements");           //W5D1 (0:10:)
         addBtn.onclick = (event) => {
         console.log("clicked ADD btn");
-        const inputField = document.querySelector("#el-count-1")
+        const inputField = document.querySelector("#el-count-1")     //W5D1 (0:11:)
         console.log("Going to add this el-s: " + inputField.value);
 
         //add elements
-        const mainCont = document.querySelector("#main-cont-1");
-        for (let i = 0; i < inputField.value; i++) {
+        const mainCont = document.querySelector("#main-cont-1");     //W5D1 (0:16:)
+        for (let i = 0; i < inputField.value; i++) {                 //W5D1 (0:18:)
             //Simple example of adding an element inline without addElement function
             const newEl = document.createElement('p');
-            newEl.id = "my-id-"+ (i+1);                       //W5D1 (0:50:)
+            newEl.id = "my-id-"+ (i+1);                             //W5D1 (0:50:)
             newEl.classList.add("my-class"); //we might need different classes *hint
             //you can see how without parenthesis the innerText is not quite what we wanted : )
             //JS converts txt with number into text
             newEl.innerText = "Lorem " + i + 1;
             mainCont.appendChild(newEl);
             
-            //Full Example on how to add new elem with prop  //W5D1 (0:49:) - commented
-            // addElement(mainCont, "p", {
+            //Full Example on how to add new elem with prop         //W5D1 (0:49:)
+            // addElement(mainCont, "p", {                          //W5D1 (0:19:)
             //     content : "Lorem", 
             //     id : "el-" +i,
             //     myClasses : ['red-txt', 'my-txt']
@@ -31,16 +30,15 @@ function main() {
             }
         }
 
-        const delBtn = document.querySelector(".del-elements");   //W5D1 (0:41:)
+        const delBtn = document.querySelector(".del-elements");      //W5D1 (0:41:)
         //main difference between regular function and arrows is the scope of this
         delBtn.onclick = function (event) {
             deleteAllElements(document.querySelector("#main-cont-1"));
         }
-
 }
 
 
-function addElement (parent, tag, props) {      //W5D1 (0:25:)
+function addElement (parent, tag, props) {                          //W5D1 (0:15:)
     console.log ("Adding a new element");
     const newEl = document.createElement(tag);
     newEl.innerText = props.content;
@@ -48,13 +46,13 @@ function addElement (parent, tag, props) {      //W5D1 (0:25:)
     if (props.hasOwnProperty('myClasses ')) {
         //new ES6 spread operator destructor for our array into parameters
         //so [1, "foo", "bar"] becomes 1, "foo", "bar"
-        newEl.classList.add(...props.myClasses);      //W5D1 (0:33:)
+        newEl.classList.add(...props.myClasses);                    //W5D1 (0:33:)
     }
     parent.appendChild(newEl);
 }
 
 
-function deleteAllElements(parent) {             //W5D1 (0:37:)
+function deleteAllElements(parent) {                                //W5D1 (0:37:)
 console.log("Deleting all children");
 while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
