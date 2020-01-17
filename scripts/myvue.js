bf18b9293a2ec9aecd67a9d51eafbcc2bce7e4b4
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-01-17 22:05:25 +0200

    precise Vue lection W4D5

--- scripts/myvue.js
+++ scripts/myvue.js
@@ -1,20 +1,20 @@
-console.log("Vue script dz");       //W4D5 (2:15:)
+console.log("Vue script dz");                        //W4D5 (2:15:)
 
 const app = new Vue({
-    el : "#app1", //here we define root element where we will hold our app
+    el : "#app1", //here we define root element (parent) where we will hold our app
     data : {
         message : "RCS",
         warning : "leopard",
-        created : "This was made on " + new Date().toLocaleString(), //W4D5 (2:23:)
-        seen : true,                         //W4D5 (2:32:)
-        todos: [                             //W4D5 (2:34:)
+        created : "This was made on " + new Date().toLocaleString(), //W4D5 (2:23:)(24)
+        seen : true,                                 //W4D5 (2:32:)
+        todos: [                                     //W4D5 (2:34:)
             { text: 'Learn JavaScript' },
             { text: 'Learn Vue' },
             { text: 'Build something awesome' }
           ]
 
     },
-    methods : {                             //W4D5 (2:40:)
+    methods : {                                      //W4D5 (2:40:)
         reverseMessage : function() {
             //another bit of magic this.message is mapped to this.data.message
             console.log("Reversing Message");
@@ -25,3 +25,5 @@ const app = new Vue({
 
 
 })
+
+//  https://vuejs.org/v2/guide/
